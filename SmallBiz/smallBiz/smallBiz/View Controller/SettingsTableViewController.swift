7d72576e6e6b641b7d846e0cac60fdc70379d6bc
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-21 07:51:07 -0700

    lots of bugs to fix right now

--- SmallBiz/smallBiz/smallBiz/View Controller/SettingsTableViewController.swift
+++ SmallBiz/smallBiz/smallBiz/View Controller/SettingsTableViewController.swift
@@ -5,4 +5,31 @@
 //  Created by Nicolas Garaycochea on 11/19/22.
 //
 
-import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    // ended here project 2 part 3 number 2
+    lazy var datasource: [SettingsItem] = {
+        return [SettingsItem(settingTitle: "New Employee default items", isActive: false, type: .toggleSwitch)]
+    }()
+    
+    
+    
+}
+
+
+struct SettingsItem {
+    var settingTitle: String
+    var isActive: Bool
+    var type: SettingCellType
+}
+
+enum SettingCellType{
+    case toggleSwitch
+    case disclosure
+}
